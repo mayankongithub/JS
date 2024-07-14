2ff4a3380a2b233563aa12e4d1d2fde2bca90913
Mayank Sharma <mayank0611sharma@gmail.com>
2024-07-14 22:58:56 +0530

    JavaScript

---
+++ 01_js.js
@@ -0,0 +1,34 @@
+// let number = 1234;
+// var marks;// AGAR VALUE NI DI HAI TOH UNDEFINED AAYEGA!!
+// console.log(typeof(number));
+// let name = "Mayank Sharma";
+// console.log(name);
+// console.log(typeof(name));
+// console.table([number,name,marks]);
+// console.log(typeof underfined)
+
+// let name = "123"
+// console.log(typeof(name));
+// let typecast = Number(name);
+// console.log(typecast);
+// console.log(typeof typecast);
+
+// let name1 = "mayank123";
+// console.log(typeof name1);
+// let chan = Number(name1);
+// console.log(typeof chan);
+// console.log(chan);
+
+// CONVERSION WOH NI HO SKTHE STRING TO NUMBER MAI TOH PRINT KRNE PE NaN AATA H (NOT A NUMBER)
+
+
+// let num = 10;
+// console.log(++num);
+
+// const id = Symbol('123');
+// console.log(typeof id);
+
+
+let x = "mayank"
+console.log(x.slice(-3,3))
+console.log(x.substring(-3,3))
